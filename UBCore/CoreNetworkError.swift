e933e9977c83e87ed288890f60f38701ff4d5f17
Levan Chikvaidze <shostlycrue@gmail.com>
2019-10-09 12:06:34 +0400

    APIProvider and CoreNetworkError updates
    В APIProvider исправлена сортировка успешных статус-кодов, реализован маппинг ответов с телом из одной строки, серверные ошибки без тела мапятся в CoreNetworkError.server(code: Int).

--- UBCore/CoreNetworkError.swift
+++ UBCore/CoreNetworkError.swift
@@ -10,12 +10,41 @@
 public enum CoreNetworkError: Error {
     case reachabilityLost
     case serialization
+    case server(code: Int)
     case unknown
     
-    var localizedDescription: String {
+    public var localizedDescription: String {
         switch self {
-        case .reachabilityLost: return "Соединение разорвано. Проверьте подключение к интернету."
-        case .serialization, .unknown: return "Произошла ошибка"
+        case .reachabilityLost:
+            return "Соединение разорвано. Проверьте подключение к интернету."
+        case .serialization, .server, .unknown:
+            return "Произошла ошибка"
+        }
+    }
+    
+    public var statusCode: Int? {
+        switch self {
+        case let .server(code): return code
+        default: return .none
+        }
+    }
+}
+
+
+// MARK: - Equatable
+extension CoreNetworkError: Equatable {
+    public static func ==(lhs: CoreNetworkError, rhs: CoreNetworkError) -> Bool {
+        switch (lhs, rhs) {
+        case (.reachabilityLost, .reachabilityLost):
+            return true
+        case (.serialization, .serialization):
+            return true
+        case (.unknown, .unknown):
+            return true
+        case let (.server(lhsCode), .server(rhsCode)):
+            return lhsCode == rhsCode
+        default:
+            return false
         }
     }
 }
